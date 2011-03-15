241c538c6aecb8fbb4dbfd6ffd2c99af7ac1fe06
unknown <Keffis@.(none)>
2011-03-15 13:29:32 +0100

    fix fix fix

--- src/HareAndHounds.java
+++ src/HareAndHounds.java
@@ -6,7 +6,11 @@ public class HareAndHounds {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("KEFFIS €R B…G!");
+		while(true)
+		{
+			System.out.println("HARALD ÄR BÖG SOM FAN!!!");
+		}
+		
 	}
 
 }
