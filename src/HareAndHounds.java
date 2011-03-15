265c527d8db9b1ab7c91d72a9895e39fdb55d5d1
Harald Hartwig <harald.hartwig@gmail.com>
2011-03-15 13:25:52 +0100

    fixade lite

--- src/HareAndHounds.java
+++ src/HareAndHounds.java
@@ -6,7 +6,7 @@ public class HareAndHounds {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HARALD ÄR BÖG!");
+		System.out.println("KEFFIS €R B…G!");
 	}
 
 }
